05e54e14bbe9cb11113ca5f4339490f8fc3184ac
mvg-ljp <mvg.ljp@hotmail.com>
2024-03-06 02:35:33 -0300

    entrega hbsysockets

--- Source/app.js
+++ Source/app.js
@@ -1,21 +1,61 @@
 const express = require('express');
+const { Server } = require('socket.io');
+const handlebars = require("express-handlebars");
+const ProductsManager = require("./managers/products");
+
 const app = express();
-const productRouter = require('./Routes/products.route');
-const cartRouter = require('../Routes/carts.route');
-const multer = require('./utils');
 
-//Luego pasar a .env
-const port = 8080;
+const productsManager = new ProductsManager();
+const PORT = 3000;
 
-//Middlewares
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-//Routes
-app.use('/api/products', productRouter);
-app.use('/api/carts', cartRouter);
-
-//Listen
-app.listen(port, () => {
-    console.log(`Server running on port ${8080}`);
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on port ${server.address().port}`);
 });
+server.on("error", error => console.log(`Server error: ${error}`));
+
+const socketServer = new Server(server);
+
+app.get("/", async (req, res) => {
+    const id = req.params.id;
+    const limit = req.query.limit;
+    try {
+        if (!!id) {
+            res.send(await productsManager.getProductById(id));
+        } else {
+            return res.render("home", { products: await productsManager.getAllProducts(limit) });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: -1, description: "Error fetching products" });
+    }
+});
+
+app.get("/realtimeproducts", async (req, res) => {
+    return res.render("realtimeproducts");
+});
+
+socketServer.on('connection', (socket) => {
+    console.log('User connected');
+  
+    socket.on('new-product', async (product) => {
+        console.log('New product:', product);
+        await productsManager.saveProduct(product);
+        socket.emit('new-product-list', await productsManager.getAllProducts());
+    });
+  
+    socket.on('delete-product', (productId) => {
+        console.log('Delete product:', productId);
+    });
+  
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+app.engine("hbs", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+app.use(express.static('public'));
+
+// app.use('/static', express.static(__dirname + '/public'));
+app.use(express.json());
